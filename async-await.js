b4d7bab7b1c8886892e157b9690d1645adb910b6
Sreerag Parakkal <145093477+Sreerag-Parakkal@users.noreply.github.com>
2023-09-15 17:57:43 +0530

    Add files via upload

---
+++ async-await.js
@@ -0,0 +1,20 @@
+//? async await
+
+const getData = async ()=>{
+    var data = await 'sreerag'
+    console.log (data)
+
+}
+
+getData();
+console.log("1")
+console.log("2")
+
+
+
+
+// for write it on a typical method 
+ 
+// async function getData(){
+
+//  }
